c9794ba2b7cc62eba7855930318e874216c267bf
Insaf Khamzin <insaf.khamzin@ceridian.com>
2021-04-13 21:49:39 -0400

    Collection was added

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { Model } from './Model';
 import { Attributes } from './Attribute';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 
 export interface UserProps {
     id?: number;
@@ -12,11 +13,16 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3000/users';
 
 export class User extends Model<UserProps> {
-    static buildUser(attrs: UserProps): User{
+    static buildUser(attrs: UserProps): User {
         return new User(
             new Attributes<UserProps>(attrs),
             new Eventing(),
             new ApiSync<UserProps>(rootUrl)
         );
     };
+
+    static buildUserCollection(): Collection<User, UserProps> {
+        return new Collection<User, UserProps>
+            ('http://localhost:3000/users', (json: UserProps) => User.buildUser(json));
+    }
 }
